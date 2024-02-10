ba483eea58a7e2f27e3ce41eb1ec25e7d0fe4fcd
sampath achalla <73095490+sampathachalla@users.noreply.github.com>
2024-02-09 23:27:56 -0600

    cn_setup.sh

--- cn_setup.sh
+++ cn_setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#Storing the outputs in container_setup_output.txt file
+#Storing the output in a output file
 exec > cn_setup_output.txt 2>&1
 
 
-#For loop run based on cpu sizes that we required for creating the container
+#Running loop based on cpu size
 for cpu_size in 1 2 4 8 16 32 48; do
-    # Defining the container name based on the allocated CPU sizes
+    # intialing the container name
     container_name="CN${cpu_size}"
     
-    # Launching the container with the specified CPU count and specified memory limits
+    # Launching the container 
     echo "Creating the container with name $container_name and have $cpu_size CPU(s) "
     sudo lxc launch ubuntu:22.04 "$container_name"  -c limits.cpu="$cpu_size" -c limits.memory=150GiB
     
@@ -17,7 +17,7 @@ for cpu_size in 1 2 4 8 16 32 48; do
     echo "Waiting for the $container_name to start"
     sleep 60 
 
-    # Installing sysbench and iperf libraries inside the VM
+    # Installing sysbench and iperf libraries inside the container
     echo "Installing sysbench and iperf in $container_name..."
     sudo lxc exec "$container_name" -- sudo apt update
     sudo lxc exec "$container_name" -- sudo apt install -y sysbench iperf
